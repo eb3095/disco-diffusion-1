8938adf3637337ff540c60f3a30ee15f2b85525f
Mike Howles <entmike@gmail.com>
2022-07-18 16:18:31 -0500

    Update workermode.sh

--- docker/workermode.sh
+++ docker/workermode.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "Worker Started"
 
-cd /workspace/disco-diffusion-1
-git pull
-
 if [[ $PUBLIC_KEY ]]
 then
     mkdir -p ~/.ssh
@@ -16,4 +13,6 @@ then
     echo "SSH Service Started"
 fi
 
+cd /workspace/disco-diffusion-1
+git pull
 python workermode.py --dd_url $1 --agent $2
